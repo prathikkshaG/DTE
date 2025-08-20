aef461ba9078c34499ccb9489b17d0c5ecd5f3ed
prathikkshaG <142028590+prathikkshaG@users.noreply.github.com>
2025-08-20 18:32:24 +0530

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:5.10
+import PackageDescription
+
+let package = Package(
+    name: "SwiftConsoleApp",
+    targets: [
+        .executableTarget(
+            name: "SwiftConsoleApp",
+            path: "Sources/SwiftConsoleApp"
+        )
+    ]
+)
